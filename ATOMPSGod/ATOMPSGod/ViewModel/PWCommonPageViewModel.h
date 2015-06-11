ebbc4337054ebd20c3972db81684de81e019cc46
peiwei <cpwduck@gmail.com>
2015-06-11 16:57:15 +0800

    follow tab

---
+++ ATOMPSGod/ATOMPSGod/ViewModel/PWCommonPageViewModel.h
@@ -0,0 +1,13 @@
+//
+//  PWCommonPageViewModel.h
+//  ATOMPSGod
+//
+//  Created by Peiwei Chen on 6/11/15.
+//  Copyright (c) 2015 ATOM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PWCommonPageViewModel : NSObject
+
+@end
