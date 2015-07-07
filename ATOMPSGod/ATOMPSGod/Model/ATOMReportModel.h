acca4261038fcc4a7aea54a2cf87e696fe5d0b32
peiwei <cpwduck@gmail.com>
2015-07-07 14:18:57 +0800

    save jul 7

---
+++ ATOMPSGod/ATOMPSGod/Model/ATOMReportModel.h
@@ -0,0 +1,13 @@
+//
+//  ATOMReportModel.h
+//  ATOMPSGod
+//
+//  Created by Peiwei Chen on 7/6/15.
+//  Copyright (c) 2015 ATOM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATOMReportModel : NSObject
+
+@end
---
+++ ATOMPSGod/ATOMPSGod/Model/ATOMReportModel.m
@@ -0,0 +1,13 @@
+//
+//  ATOMReportModel.m
+//  ATOMPSGod
+//
+//  Created by Peiwei Chen on 7/6/15.
+//  Copyright (c) 2015 ATOM. All rights reserved.
+//
+
+#import "ATOMReportModel.h"
+
+@implementation ATOMReportModel
+
+@end
