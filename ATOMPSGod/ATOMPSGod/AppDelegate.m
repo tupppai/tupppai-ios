497a83b8f4d745c393d7a98ab480167e71a0c180
peiwei <cpwduck@gmail.com>
2015-08-06 19:06:46 +0800

    add introduction pages

--- ATOMPSGod/ATOMPSGod/AppDelegate.m
+++ ATOMPSGod/ATOMPSGod/AppDelegate.m
@@ -49,23 +49,7 @@
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= _IPHONE80_
     if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
     {
-        //register remoteNotification types （iOS 8.0及其以上版本）
-//        UIMutableUserNotificationAction *action1 = [[UIMutableUserNotificationAction alloc] init];
-//        action1.identifier = @"action1_identifier";
-//        action1.title=@"Accept";
-//        action1.activationMode = UIUserNotificationActivationModeForeground;//当点击的时候启动程序
-//        
-//        UIMutableUserNotificationAction *action2 = [[UIMutableUserNotificationAction alloc] init];  //第二按钮
-//        action2.identifier = @"action2_identifier";
-//        action2.title=@"Reject";
-//        action2.activationMode = UIUserNotificationActivationModeBackground;//当点击的时候不启动程序，在后台处理
-//        action2.authenticationRequired = YES;//需要解锁才能处理，如果action.activationMode = UIUserNotificationActivationModeForeground;则这个属性被忽略；
-//        action2.destructive = YES;
-//        
-//        UIMutableUserNotificationCategory *categorys = [[UIMutableUserNotificationCategory alloc] init];
-//        categorys.identifier = @"category1";//这组动作的唯一标示
-//        [categorys setActions:@[action1,action2] forContext:(UIUserNotificationActionContextDefault)];
-        
+
         UIUserNotificationSettings *userSettings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge|UIUserNotificationTypeSound|UIUserNotificationTypeAlert
                                                                                      categories:nil];
         [UMessage registerRemoteNotificationAndUserNotificationSettings:userSettings];
@@ -75,7 +59,6 @@
         [UMessage registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge
          |UIRemoteNotificationTypeSound
          |UIRemoteNotificationTypeAlert];
-
     }
 #else
     
@@ -89,29 +72,30 @@
     [UMessage setLogEnabled:YES];
 }
 -(void)initializeAfterDB {
-    ATOMIntroductionOnFirstLaunchViewController* vc = [ATOMIntroductionOnFirstLaunchViewController new];
-        self.baseNav = [[ATOMLoginCustomNavigationController alloc] initWithRootViewController:vc];
-        self.window.rootViewController = self.baseNav;
-        [self.window makeKeyAndVisible];
 
-//    [[ATOMCurrentUser currentUser]fetchCurrentUserInDB:^(BOOL hasCurrentUser) {
-//        if (hasCurrentUser) {
-//            self.window.rootViewController = self.mainTabBarController;
-//        } else {
-//            
-//            if (![[NSUserDefaults standardUserDefaults] boolForKey:@"HasLaunchedOnce"])
-//            {
-//                //UIPageViewController
-//                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"HasLaunchedOnce"];
-//                [[NSUserDefaults standardUserDefaults] synchronize];
-//            } else {
-//                ATOMLaunchViewController *lvc = [[ATOMLaunchViewController alloc] init];
-//                self.baseNav = [[ATOMLoginCustomNavigationController alloc] initWithRootViewController:lvc];
-//                self.window.rootViewController = self.baseNav;
-//            }
-//        }
-//        [self.window makeKeyAndVisible];
-//    }];
+
+    [[ATOMCurrentUser currentUser]fetchCurrentUserInDB:^(BOOL hasCurrentUser) {
+        if (hasCurrentUser) {
+            self.window.rootViewController = self.mainTabBarController;
+        } else {
+            
+            if (![[NSUserDefaults standardUserDefaults] boolForKey:@"HasLaunchedOnce"])
+            {
+                //UIPageViewController
+                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"HasLaunchedOnce"];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+                ATOMIntroductionOnFirstLaunchViewController* vc = [ATOMIntroductionOnFirstLaunchViewController new];
+                self.baseNav = [[ATOMLoginCustomNavigationController alloc] initWithRootViewController:vc];
+                self.window.rootViewController = self.baseNav;
+                [self.window makeKeyAndVisible];
+            } else {
+                ATOMLaunchViewController *lvc = [[ATOMLaunchViewController alloc] init];
+                self.baseNav = [[ATOMLoginCustomNavigationController alloc] initWithRootViewController:lvc];
+                self.window.rootViewController = self.baseNav;
+            }
+        }
+        [self.window makeKeyAndVisible];
+    }];
 }
 
 -(void)setupNotification {
--- ATOMPSGod/ATOMPSGod/Controller/login/ATOMIntroductionOnFirstLaunchViewController.m
+++ ATOMPSGod/ATOMPSGod/Controller/login/ATOMIntroductionOnFirstLaunchViewController.m
@@ -8,7 +8,7 @@
 
 #import "ATOMIntroductionOnFirstLaunchViewController.h"
 #import "EAIntroView.h"
-
+#import "ATOMLaunchViewController.h"
 @interface ATOMIntroductionOnFirstLaunchViewController ()<EAIntroDelegate>
 
 @end
@@ -41,29 +41,30 @@
     
     EAIntroPage *page2 = [EAIntroPage page];
     page2.title = @"This is page 2";
-    page2.bgImage = [UIImage imageNamed:@""];
+    page2.bgImage = [UIImage imageNamed:@"intro2.jpg"];
     page2.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title2"]];
     
     EAIntroPage *page3 = [EAIntroPage page];
     page3.title = @"This is page 3";
     page3.desc = @"love you";
-    page3.bgImage = [UIImage imageNamed:@"back"];
+    page3.bgImage = [UIImage imageNamed:@"intro3.jpg"];
     page3.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title3"]];
     
     EAIntroPage *page4 = [EAIntroPage page];
     page4.title = @"This is page 4";
-    page4.bgImage = [UIImage imageNamed:@"intro4"];
+    page4.bgImage = [UIImage imageNamed:@"intro4.jpg"];
     page4.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title4"]];
 
     EAIntroView *intro = [[EAIntroView alloc] initWithFrame:self.view.bounds andPages:@[page1,page2,page3,page4]];
     [intro setDelegate:self];
-    intro.backgroundColor = [UIColor lightGrayColor];
-    NSLog(@"self %@",NSStringFromCGRect(self.view.bounds));
-
-    NSLog(@"intro%@",NSStringFromCGRect(intro.bounds));
-
     [intro showInView:self.view animateDuration:0.3];
 
+}
+#pragma mark - EAIntroView delegate
+
+- (void)introDidFinish:(EAIntroView *)introView {
+    ATOMLaunchViewController* lvc = [ATOMLaunchViewController new];
+    self.navigationController.viewControllers = @[lvc];
 }
 
 
