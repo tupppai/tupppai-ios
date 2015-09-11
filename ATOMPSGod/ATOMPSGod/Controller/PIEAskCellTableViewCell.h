4e63f939e6305e633aac706a263bb64299bae217
peiwei <peiwei.chen8@gmail.com>
2015-09-11 11:28:18 +0800

    transfer data to new mac

---
+++ ATOMPSGod/ATOMPSGod/Controller/PIEAskCellTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PIEAskCellTableViewCell.h
+//  
+//
+//  Created by chenpeiwei on 9/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PIEAskCellTableViewCell : UITableViewCell
+
+@end
---
+++ ATOMPSGod/ATOMPSGod/Controller/PIEAskCellTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PIEAskCellTableViewCell.m
+//  
+//
+//  Created by chenpeiwei on 9/11/15.
+//
+//
+
+#import "PIEAskCellTableViewCell.h"
+
+@implementation PIEAskCellTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
