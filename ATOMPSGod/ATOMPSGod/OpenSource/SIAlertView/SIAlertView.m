29733a6ed70af5704ed7abc4b225ddfdba7fc79b
peiwei <payway@foxmail.com>
2015-10-28 13:17:33 +0800

    fix all warnings

--- ATOMPSGod/ATOMPSGod/OpenSource/SIAlertView/SIAlertView.m
+++ ATOMPSGod/ATOMPSGod/OpenSource/SIAlertView/SIAlertView.m
@@ -177,8 +177,7 @@ static SIAlertView *__si_alert_current_view;
 }
 #endif
 
-- (NSUInteger)supportedInterfaceOrientations
-{
+-(UIInterfaceOrientationMask)supportedInterfaceOrientations {
     UIViewController *viewController = [self.alertView.oldKeyWindow currentViewController];
     if (viewController) {
         return [viewController supportedInterfaceOrientations];
--- ATOMPSGod/ATOMPSGod/OpenSource/TSMessage/Classes/TSMessage.m
+++ ATOMPSGod/ATOMPSGod/OpenSource/TSMessage/Classes/TSMessage.m
@@ -209,7 +209,7 @@ __weak static UIViewController *_defaultViewController;
         else
             currentNavigationController = (UINavigationController *)currentView.viewController.parentViewController;
         
-        BOOL isViewIsUnderStatusBar = [[[currentNavigationController childViewControllers] firstObject] wantsFullScreenLayout];
+        BOOL isViewIsUnderStatusBar = YES;
         if (!isViewIsUnderStatusBar && currentNavigationController.parentViewController == nil) {
             isViewIsUnderStatusBar = ![TSMessage isNavigationBarInNavigationControllerHidden:currentNavigationController]; // strange but true
         }
