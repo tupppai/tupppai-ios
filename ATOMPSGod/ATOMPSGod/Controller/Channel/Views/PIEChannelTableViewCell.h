113703ad858a9326b0376e30e1f6ba9c3112d20e
黄炜 <harw.op@qq.com>
2015-12-07 13:54:30 +0800

    Added PIEChannelBannerViewController, along with PIEChannelBannerCell and PIEChannelTableViewCell

---
+++ ATOMPSGod/ATOMPSGod/Controller/Channel/Views/PIEChannelTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PIEChannelTableViewCell.h
+//  TUPAI-DEMO
+//
+//  Created by huangwei on 15/12/4.
+//  Copyright (c) 2015年 huangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PIEChannelTableViewCell : UITableViewCell
+
+@end
