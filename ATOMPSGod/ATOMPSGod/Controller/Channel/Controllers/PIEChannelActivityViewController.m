066e5969a7b1a492e502ad3751a8a9e28cfad505
黄炜 <harw.op@qq.com>
2015-12-11 15:28:58 +0800

    PIEChannelActivityViewController + replyCell的各个元素的点击事件 + shareView的8个delegate方法

--- ATOMPSGod/ATOMPSGod/Controller/Channel/Controllers/PIEChannelActivityViewController.m
+++ ATOMPSGod/ATOMPSGod/Controller/Channel/Controllers/PIEChannelActivityViewController.m
@@ -12,6 +12,12 @@
 #import "PIEPageVM.h"
 #import "PIEChannelViewModel.h"
 #import "PIEChannelManager.h"
+#import "PIECarouselViewController2.h"
+#import "PIEFriendViewController.h"
+#import "PIECommentViewController.h"
+#import "PIEReplyCollectionViewController.h"
+#import "PIEShareView.h"
+#import "DDCollectManager.h"
 
 
 /* Variables */
@@ -32,6 +38,15 @@
 /* HTTP Request parameter */
 @property (nonatomic, assign) long long timeStamp;
 
+/* ======= */
+
+@property (nonatomic, strong) NSIndexPath *selectedIndexPath;
+
+@property (nonatomic, strong) PIENewReplyTableCell *selectedReplyCell;
+
+/** 点击弹出的分享页面 */
+@property (nonatomic, strong) PIEShareView *shareView;
+
 @end
 
 /* Protocols */
@@ -43,6 +58,10 @@
 <PWRefreshBaseTableViewDelegate>
 @end
 
+@interface PIEChannelActivityViewController (SharingDelegate)
+<PIEShareViewDelegate>
+
+@end
 
 @implementation PIEChannelActivityViewController
 
@@ -67,6 +86,9 @@ PIEChannelActivityNormalCellIdentifier = @"PIEChannelActivityNormalCellIdentifie
     // configure subviews
     [self configureTableView];
     [self configureGoPsButton];
+    
+    /* 设置可以区分reply cell中不同UI元素（头像，关注按钮，分享, etc.）的点击事件回调 */
+    [self setupGestures];
     
     // load data for the first time.
     [self.tableView.mj_header beginRefreshing];
@@ -239,6 +261,283 @@ PIEChannelActivityNormalCellIdentifier = @"PIEChannelActivityNormalCellIdentifie
 
 }
 
+#pragma mark - Gesture Event - 识别PIENewReplyCell中的不同元素(头像，关注按钮，etc.)的点击事件
+
+- (void)setupGestures {
+    UITapGestureRecognizer* tapGestureReply =
+    [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapOnReply:)];
+    UILongPressGestureRecognizer* longPressGestureReply =
+    [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(longPressOnReply:)];
+    [self.tableView addGestureRecognizer:longPressGestureReply];
+    [self.tableView addGestureRecognizer:tapGestureReply];
+    
+}
+
+- (void)tapOnReply:(UITapGestureRecognizer *)gesture {
+    
+    PIELog(@"%s", __func__);
+    
+    CGPoint location = [gesture locationInView:self.tableView];
+    _selectedIndexPath = [self.tableView indexPathForRowAtPoint:location];
+    if (_selectedIndexPath) {
+        _selectedReplyCell = [self.tableView cellForRowAtIndexPath:_selectedIndexPath];
+        _selectedVM = self.source_reply[_selectedIndexPath.row];
+        CGPoint p = [gesture locationInView:_selectedReplyCell];
+        
+        //点击小图
+        if (CGRectContainsPoint(_selectedReplyCell.thumbView.frame, p)) {
+            CGPoint pp = [gesture locationInView:_selectedReplyCell.thumbView];
+            if (CGRectContainsPoint(_selectedReplyCell.thumbView.leftView.frame,pp)) {
+                [_selectedReplyCell animateThumbScale:PIEAnimateViewTypeLeft];
+            }
+            else if (CGRectContainsPoint(_selectedReplyCell.thumbView.rightView.frame,pp)) {
+                [_selectedReplyCell animateThumbScale:PIEAnimateViewTypeRight];
+            }
+        }
+        //点击大图
+        else  if (CGRectContainsPoint(_selectedReplyCell.theImageView.frame, p)) {
+            //进入热门详情
+            PIECarouselViewController2* vc = [PIECarouselViewController2 new];
+            _selectedVM.image = _selectedReplyCell.theImageView.image;
+            vc.pageVM = _selectedVM;
+            [self presentViewController:vc animated:YES completion:nil];
+            //                [self.navigationController pushViewController:vc animated:YES];
+        }
+        //点击头像
+        else if (CGRectContainsPoint(_selectedReplyCell.avatarView.frame, p)) {
+            PIEFriendViewController * friendVC = [PIEFriendViewController new];
+            friendVC.pageVM = _selectedVM;
+            [self.navigationController pushViewController:friendVC animated:YES];
+        }
+        //点击用户名
+        else if (CGRectContainsPoint(_selectedReplyCell.nameLabel.frame, p)) {
+            PIEFriendViewController * friendVC = [PIEFriendViewController new];
+            friendVC.pageVM = _selectedVM;
+            [self.navigationController pushViewController:friendVC animated:YES];
+        }
+        //            else if (CGRectContainsPoint(_selectedReplyCell.collectView.frame, p)) {
+        //                //should write this logic in viewModel
+        ////                [self collect:_selectedReplyCell.collectView shouldShowHud:NO];
+        //                [self collect];
+        //            }
+        else if (CGRectContainsPoint(_selectedReplyCell.likeView.frame, p)) {
+            [self likeReply];
+        }
+        else if (CGRectContainsPoint(_selectedReplyCell.followView.frame, p)) {
+            [self followReplier];
+        }
+        else if (CGRectContainsPoint(_selectedReplyCell.shareView.frame, p)) {
+            [self showShareView];
+        }
+        else if (CGRectContainsPoint(_selectedReplyCell.commentView.frame, p)) {
+            PIECommentViewController* vc = [PIECommentViewController new];
+            vc.vm = _selectedVM;
+            vc.shouldShowHeaderView = NO;
+            [self.navigationController pushViewController:vc animated:YES];
+        }
+        else if (CGRectContainsPoint(_selectedReplyCell.allWorkView.frame, p)) {
+            PIEReplyCollectionViewController* vc = [PIEReplyCollectionViewController new];
+            vc.pageVM = _selectedVM;
+            [self.navigationController pushViewController:vc animated:YES];
+        }
+    }
+    
+}
+- (void)longPressOnReply:(UILongPressGestureRecognizer *)gesture {
+    
+    PIELog(@"%s", __func__);
+    
+    CGPoint location   = [gesture locationInView:self.tableView];
+    _selectedIndexPath = [self.tableView indexPathForRowAtPoint:location];
+    if (_selectedIndexPath) {
+        _selectedReplyCell = [self.tableView cellForRowAtIndexPath:_selectedIndexPath];
+        _selectedVM        = self.source_reply[_selectedIndexPath.row];
+        CGPoint p          = [gesture locationInView:_selectedReplyCell];
+        
+        //点击大图
+        if (CGRectContainsPoint(_selectedReplyCell.theImageView.frame, p)) {
+            [self showShareView];
+        }
+    }
+    
+    
+}
+
+#pragma mark - reply cell 中的点击事件：喜欢该P图，关注P图主。
+
+/**
+ *  点击事件： 喜欢这张P图
+ */
+-(void)likeReply {
+    
+    PIELog(@"%s", __func__);
+    
+    
+    _selectedReplyCell.likeView.selected = !_selectedReplyCell.likeView.selected;
+    [DDService toggleLike:_selectedReplyCell.likeView.selected ID:_selectedVM.ID type:_selectedVM.type  withBlock:^(BOOL success) {
+        if (success) {
+            _selectedVM.liked = _selectedReplyCell.likeView.selected;
+            if (_selectedReplyCell.likeView.selected) {
+                _selectedVM.likeCount = [NSString stringWithFormat:@"%zd",_selectedVM.likeCount.integerValue + 1];
+            } else {
+                _selectedVM.likeCount = [NSString stringWithFormat:@"%zd",_selectedVM.likeCount.integerValue - 1];
+            }
+        } else {
+            _selectedReplyCell.likeView.selected = !_selectedReplyCell.likeView.selected;
+        }
+    }];
+}
+
+/**
+ *  关注这张P图的P图主
+ */
+-(void)followReplier {
+    
+    PIELog(@"%s", __func__);
+    
+    
+    _selectedReplyCell.followView.highlighted = !_selectedReplyCell.followView.highlighted;
+    NSMutableDictionary *param = [NSMutableDictionary new];
+    [param setObject:@(_selectedVM.userID) forKey:@"uid"];
+    if (_selectedReplyCell.followView.highlighted) {
+        [param setObject:@1 forKey:@"status"];
+    }
+    else {
+        [param setObject:@0 forKey:@"status"];
+    }
+    [DDService follow:param withBlock:^(BOOL success) {
+        if (success) {
+            _selectedVM.followed = _selectedReplyCell.followView.highlighted;
+        } else {
+            _selectedReplyCell.followView.highlighted = !_selectedReplyCell.followView.highlighted;
+        }
+    }];
+}
+
+
+#pragma mark - Sharing-related method
+#pragma mark - methods on Sharing<ATOMShareViewDelegate>
+- (void)updateShareStatus {
+    
+    /**
+     *  用户点击了updateShareStatus之后（在弹出的窗口完成分享，点赞），刷新本页面的点赞数和分享数（两个页面的UI元素的同步）
+     */
+    _selectedVM.shareCount = [NSString stringWithFormat:@"%zd",[_selectedVM.shareCount integerValue]+1];
+    [self updateStatus];
+}
+
+- (void)showShareView {
+    [self.shareView show];
+    
+}
+
+/**
+ *  用户点击了updateShareStatus之后（在弹出的窗口完成分享，点赞），刷新本页面的点赞数和分享数
+ */
+- (void)updateStatus {
+    if (_selectedIndexPath) {
+        [self.tableView reloadRowsAtIndexPaths:@[_selectedIndexPath] withRowAnimation:UITableViewRowAnimationNone];
+    }
+}
+
+#pragma mark - <PIEShareViewDelegate>
+/*
+ 以下代理方法在用户点击了shareView中的8个button的其中一个（分享到新浪，微信，微博，etc.) 的时候被调用
+ */
+
+//sina
+-(void)tapShare1 {
+    PIELog(@"%s", __func__);
+    
+    [DDShareManager postSocialShare2:_selectedVM withSocialShareType:ATOMShareTypeSinaWeibo block:^(BOOL success) {if (success) {[self updateShareStatus];}}];
+}
+//qqzone
+-(void)tapShare2 {
+    PIELog(@"%s", __func__);
+    
+    [DDShareManager postSocialShare2:_selectedVM withSocialShareType:ATOMShareTypeQQZone block:^(BOOL success) {if (success) {[self updateShareStatus];}}];
+}
+//wechat moments
+-(void)tapShare3 {
+    PIELog(@"%s", __func__);
+    
+    [DDShareManager postSocialShare2:_selectedVM withSocialShareType:ATOMShareTypeWechatMoments block:^(BOOL success) {if (success) {[self updateShareStatus];}}];
+}
+//wechat friends
+-(void)tapShare4 {
+    PIELog(@"%s", __func__);
+    
+    [DDShareManager postSocialShare2:_selectedVM withSocialShareType:ATOMShareTypeWechatFriends block:^(BOOL success) {if (success) {[self updateShareStatus];}}];
+}
+-(void)tapShare5 {
+    PIELog(@"%s", __func__);
+    
+    [DDShareManager postSocialShare2:_selectedVM withSocialShareType:ATOMShareTypeQQFriends block:^(BOOL success) {if (success) {[self updateShareStatus];}}];
+    
+}
+
+-(void)tapShare6 {
+    PIELog(@"%s", __func__);
+    
+    [DDShareManager copy:_selectedVM];
+}
+-(void)tapShare7 {
+    PIELog(@"%s", __func__);
+    
+    
+    self.shareView.vm = _selectedVM;
+}
+-(void)tapShare8 {
+    PIELog(@"%s", __func__);
+    
+    
+    //    if (_scrollView.type == PIENewScrollTypeAsk) {
+    //        if (_selectedVM.type == PIEPageTypeAsk) {
+    [self collect];
+    //        }
+    //    } else {
+    //        if (_selectedVM.type == PIEPageTypeAsk) {
+    //            [self collect];
+    //        } else {
+    //            PIENewReplyTableCell* cell = [_scrollView.tableReply cellForRowAtIndexPath:_selectedIndexPath];
+    //            [self collect:cell.collectView shouldShowHud:YES];
+    //        }
+    //    }
+    
+}
+
+-(void)tapShareCancel {
+    PIELog(@"%s", __func__);
+    
+    [self.shareView dismiss];
+}
+
+#pragma mark - "收藏"相关操作， shareView中用户点击了"收藏"之后被调用
+-(void)collect {
+    NSMutableDictionary *param = [NSMutableDictionary new];
+    _selectedVM.collected = !_selectedVM.collected;
+    if (_selectedVM.collected) {
+        //收藏
+        [param setObject:@(1) forKey:@"status"];
+    } else {
+        //取消收藏
+        [param setObject:@(0) forKey:@"status"];
+    }
+    [DDCollectManager toggleCollect:param withPageType:_selectedVM.type withID:_selectedVM.ID withBlock:^(NSError *error) {
+        if (!error) {
+            if (  _selectedVM.collected) {
+                [Hud textWithLightBackground:@"收藏成功"];
+            } else {
+                [Hud textWithLightBackground:@"取消收藏成功"];
+            }
+        }   else {
+            _selectedVM.collected = !_selectedVM.collected;
+        }
+    }];
+}
+
+
+
 #pragma mark - Lazy loadings
 - (PIERefreshTableView *)tableView
 {
@@ -318,6 +617,17 @@ PIEChannelActivityNormalCellIdentifier = @"PIEChannelActivityNormalCellIdentifie
     }
     return _goPsButton;
 
+}
+
+- (PIEShareView *)shareView
+{
+    if (_shareView == nil) {
+        // instantiate only for once
+        _shareView = [[PIEShareView alloc] init];
+        
+        _shareView.delegate = self;
+    }
+    return  _shareView;
 }
 
 @end
