c5704b4b56fdfc90d8d7654d72ee14912ec8da22
TUPAI-Huangwei <tupai-huangwei@tupai-huangweidemacbook-pro.local>
2015-12-14 09:35:08 +0800

    testing on branching.

--- ATOMPSGod/ATOMPSGod/Controller/Channel/Views/PIEChannelTableViewCell.m
+++ ATOMPSGod/ATOMPSGod/Controller/Channel/Views/PIEChannelTableViewCell.m
@@ -14,6 +14,7 @@
     // Initialization code
     
 //    self.selectionStyle = UITableViewCellSelectionStyleNone;
+    // testing...
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
