3dc207317df810300dbe97f58188c8665255bd14
TUPAI-Huangwei <tupai-huangwei@tupai-huangweidemacbook-pro.local>
2015-12-28 17:37:30 +0800

    (需求)在PIEFriendViewController的navigationBar中添加关注与取消关注的按钮；在PIEEliteViewController中测试可以同步关注状态（从后往前需要重新刷新）

--- ATOMPSGod/ATOMPSGod/Controller/attention/PIEEliteFollowViewController.m
+++ ATOMPSGod/ATOMPSGod/Controller/attention/PIEEliteFollowViewController.m
@@ -260,6 +260,12 @@ static  NSString* replyIndentifier    = @"PIEEliteFollowReplyTableViewCell";
             _selectedVM.followed = followView.highlighted;
         } else {
             followView.highlighted = !followView.highlighted;
+            [Hud text:@"网络异常，请稍后再试"];
+        }
+        if (followView.highlighted) {
+            [Hud text:@"关注成功"];
+        }else{
+            [Hud text:@"已取消关注"];
         }
     }];
 }
