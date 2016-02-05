22e23ad3623d2519e35a85ea45c5bf4c4effd85d
TUPAI-Huangwei <tupai-huangwei@tupai-huangweidemacbook-pro.local>
2016-02-05 12:46:24 +0800

    (需求)我的任务 上拉只显示当前任务、下拉才会显示历史任务

---
+++ ATOMPSGod/ATOMPSGod/Controller/proceeding/toHelp/PIEProceedingToHelpHeaderView_undone.h
@@ -0,0 +1,16 @@
+//
+//  PIEProceedingToHelpHeaderView_undone.h
+//  TUPAI
+//
+//  Created by TUPAI-Huangwei on 2/5/16.
+//  Copyright © 2016 Shenzhen Pires Internet Technology CO.,LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PIEProceedingToHelpHeaderView_undone : UIView
+
+
++ (PIEProceedingToHelpHeaderView_undone *)headerView;
+
+@end
