582999e0b4a0428ad6d9af0403391ce48ba3704e
TUPAI-Huangwei <tupai-huangwei@TUPAI-HuangweideMacBook-Pro.local>
2016-02-22 10:22:04 +0800

    for merging

---
+++ ATOMPSGod/ATOMPSGod/Controller/attention/PIEEliteReplyTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PIEEliteReplyTableViewCell.m
+//  TUPAI
+//
+//  Created by TUPAI-Huangwei on 2/20/16.
+//  Copyright © 2016 Shenzhen Pires Internet Technology CO.,LTD. All rights reserved.
+//
+
+#import "PIEEliteReplyTableViewCell.h"
+
+@implementation PIEEliteReplyTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
